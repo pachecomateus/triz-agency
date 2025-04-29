6dee45f5e4467223b35c89fd5abedb18b03f34a9
Mateus Pacheco <mateuspachecosete@gmail.com>
2025-04-29 19:24:49 -0300

    feat: colors nad scrollbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,23 @@ export default function Home() {
   return (
     <main>
       <Video />
+      <p>Hello World</p>
+      <p>Hello World</p>
+      <p>Hello World</p>
+
+      <p>Hello World</p>
+      <p>Hello World</p>
+      <p>Hello World</p>
+      <p>Hello World</p>
+      <p>Hello World</p>
+      <p>Hello World</p>
+      <p>Hello World</p>
+      <p>Hello World</p>
+      <p>Hello World</p>
+      <p>Hello World</p>
+      <p>Hello World</p>
+      <p>Hello World</p>
+v
     </main>
   );
 }
--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -15,11 +15,11 @@ export default function Video({className}: Readonly<{className?: string}>) {
   }, []);
 
   return (
-    <section className={`${className} h-screen w-screen bg-purple`}>
+    <section className={`${className} h-screen w-screen bg-gray`}>
       {isMobile === false && (
         <video
           src={"/videos/video-home.mp4"}
-          // poster={"/images"}
+          poster={"/images/home-inicial.png"}
           className="absolute inset-0 z-30 h-full w-full object-cover hidden lg:block"
           autoPlay
           playsInline
@@ -44,7 +44,7 @@ export default function Video({className}: Readonly<{className?: string}>) {
           loop
         />
       )}
-      <h1 className="sr-only">Agência Newe</h1>
+      <h1 className="sr-only">Triz Agency</h1>
       <div className="absolute w-full bottom-4 left-0 right-0 mx-auto z-30 justify-center lg:flex hidden">
         <div className="mousey">
           <div className="scroller"></div>
