801029d9ccc2672b4a88fdae95eb6e76287f22a0
pachecomateus <mateuspachecosete@gmail.com>
2025-05-05 11:57:59 -0300

    wip: about

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
+import About from "@/components/About";
 import Video from "@/components/Video";
 
 export default function Home() {
   return (
     <main>
       <Video />
+      <About />
       <p>Hello World</p>
       <p>Hello World</p>
       <p>Hello World</p>
-
       <p>Hello World</p>
       <p>Hello World</p>
       <p>Hello World</p>
---
+++ src/components/About.tsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+
+export default function About() {
+  return (
+    <section className="flex lg:flex-row flex-col bg-gray py-[150px]">
+        <div className="relative lg:w-1/2 h-auto">
+            <Image src="/images/logo-colorido.png" alt="Logo Triz" fill quality={100} className="object-contain" />
+        </div>
+        <div className="relative lg:w-1/2 h-full">
+            <p className="text-white text-xl">
+                <strong className="text-2xl">Triz é o instante onde tudo começa.</strong>
+                <br /> <br />
+                A faísca antes do fogo, o clique antes da transformação.
+                <br /> <br />
+                A gente não espera a ideia vir — a gente vive no ponto exato onde ela nasce.
+                <br /> <br />
+                É no agora que a gente encontra o novo. No movimento, 
+                na conexão.
+                <br /> <br />
+                Porque a criatividade verdadeira não vem do acaso. 
+                Ela é provocada. Ela é cultivada.
+                <br /> <br />
+                <strong className="text-2xl">Triz é isso:</strong>
+                <br /> <br />
+                Um estado criativo. Um campo de ignição. 
+                <br /> <br />
+                Um convite à virada. O clique inicial.
+            </p>
+        </div>
+    </section>
+  );
+}
