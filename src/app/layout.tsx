1d98318755f1c85863518bec12953b69d0f3d890
pachecomateus <mateuspachecosete@gmail.com>
2025-05-07 19:19:30 -0300

    feat: meta script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Script from "next/script";
 
 const mundial = localFont({
   src: "../fonts/mundial-regular.woff2",
@@ -26,6 +27,23 @@ export default function RootLayout({
       >
         {children}
       </body>
+      <Script
+        strategy="lazyOnload" 
+        dangerouslySetInnerHTML={{
+        __html: `
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '1050348320347233');
+          fbq('track', 'PageView');
+        `,
+        }} 
+      />
     </html>
   );
 }
